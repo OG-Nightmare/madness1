033eb053558535faa34e05b86aa450c5473f3378
Sarthak4255 <sarthaksinghk@gmail.com>
2024-12-28 09:42:22 +0530

    nav build error resolved

--- src/assets/Nav.js
+++ src/assets/Nav.js
@@ -8,28 +8,30 @@ const Nav = () => {
           <img src={logo} alt="Logo" className="log" />
         </h1>
 
-        
-        <textarea className="textarea" name="search" placeholder="search tera bhai kareg"></textarea>
+        <textarea
+          className="textarea"
+          name="search"
+          placeholder="search tera bhai kareg"
+        ></textarea>
 
         <ul className="naa">
           <li>
-            <a href="#">Home</a>
+            <a href="https://yournxtstartup.com/">Home</a>
           </li>
           <li>
-            <a href="#">About Us</a>
+            <a href="https://yournxtstartup.com/">About Us</a>
           </li>
           <li>
-            <a href="#">Products</a>
+            <a href="https://yournxtstartup.com/">Products</a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a href="https://yournxtstartup.com/">Contact</a>
           </li>{" "}
           {/* Fixed typo */}
         </ul>
       </nav>
     </>
   );
-  <img src="" alt="" />
 };
 
 export default Nav;
