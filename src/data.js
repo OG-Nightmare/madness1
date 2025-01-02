92c3efe63e9a5a3dc2ce63e848af97f866cf177e
Sarthak4255 <sarthaksinghk@gmail.com>
2025-01-02 22:47:45 +0530

    very minor card improvements

--- src/data.js
+++ src/data.js
@@ -3,7 +3,17 @@ import img2 from "./pics/henry-be-d8woejhlLlA-unsplash.jpg";
 import img3 from "./pics/pablo-merchan-montes-MXovqM130UI-unsplash.jpg";
 import img4 from "./pics/pexels-ella-olsson-572949-1640773.jpg";
 import img5 from "./pics/tangerine-newt-ogqaFRY7hAc-unsplash.jpg";
-
+import img6 from "./pics/pexels-aibek-skakov-418917601-29957494.jpg"
+import img7 from "./pics/pexels-alexandra-stroescu-1044324559-29021173.jpg"
+import img8 from "./pics/pexels-ali-dashti-506667798-17649371.jpg"
+import img9 from "./pics/pexels-dana-tentis-118658-691114.jpg"
+import img10 from "./pics/pexels-eat-kubba-15863701-11842142.jpg"
+import img11 from "./pics/pexels-fariphotography-905847.jpg"
+import img12 from "./pics/pexels-huzaifabukhari-16845335.jpg"
+import img13 from "./pics/pexels-lum3n-44775-1410235.jpg"
+import img14 from "./pics/pexels-olgalioncat-7245484.jpg"
+import img15 from "./pics/pexels-renestrgar-13814644.jpg"
+import img16 from "./pics/pexels-vince-2147491.jpg"
 
 
 export const restos = [
@@ -12,6 +22,15 @@ export const restos = [
   { img: img3, name: "M2", cusine: "", price: "" },
   { img: img4, name: "M8", cusine: "", price: "" },
   { img: img5, name: "Macan", cusine: "", price: "" },
-//   { img: "", name: "corvet", cusine: "", price: "" },
-//   { img: "", name: "Mustang", cusine: "", price: "" },
+  { img: img6, name: "corvet", cusine: "", price: "" },
+  { img: img7, name: "Mustang", cusine: "", price: "" },
+  { img: img8, name: "Camaro", cusine: "", price: "" },
+  { img: img9, name: "Challenger", cusine: "", price: "" },
+  { img: img10, name: "Charger", cusine: "", price: "" },
+  { img: img11, name: "Civic", cusine: "", price: "" },
+  { img: img12, name: "Accord", cusine: "", price: "" },
+  { img: img13, name: "Pagani ", cusine: "", price: "" },
+  { img: img14, name: "Revolto", cusine: "", price: "" },
+  { img: img15, name: "Lexus LFA", cusine: "", price: "" },
+  { img: img16, name: "Panamara", cusine: "", price: "" }
 ];
